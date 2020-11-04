c83e2d1e021a783ed15aa126b170ac6076c86a02
lcyzgdy <MyGuanDY@outlook.com>
2020-11-04 23:38:45 +0800

    简单的promise

---
+++ primise-cpp.test/primise-cpp.test.cpp
@@ -0,0 +1,14 @@
+﻿// primise-cpp.test.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+
+#include "Promise.hpp"
+
+int main()
+{
+	Promise::Promise<void> a([](std::function<void()>&& resolve)->void {
+		std::cout << "Hello World!\n";
+		resolve();
+	});
+}
---
+++ promise-cpp/Promise.hpp
@@ -0,0 +1,41 @@
+﻿#pragma once
+#ifndef __PROMISE_HPP__
+#define __PROMISE_HPP__
+
+#include <functional>
+
+namespace Promise
+{
+	enum class PromiseStatus
+	{
+		ePending,
+		eFulfilled,
+		eRejected,
+
+		ePromiseStatusCount
+	};
+
+	template<typename _Ty>
+	class Promise
+	{
+		using _ResolveFuncType = std::function<void(_Ty)>;
+
+		PromiseStatus m_status;
+
+		inline void DefaultResolve(_Ty&& value) { this->m_status = PromiseStatus::eFulfilled; }
+		inline void DefaultReject(std::exception&& e) { this->m_status = PromiseStatus::eRejected; }
+
+	public:
+		Promise(std::function<void(_ResolveFuncType&&)>&& func)
+		{
+			func(std::bind(&DefaultResolve, this, std::placeholders::_1));
+		}
+		/*
+		template<typename _ThenRet>
+		Promise<_ThenRet>& Then(std::function<_ThenRet(_Ty)>&& func)
+		{
+		}
+		*/
+	};
+}
+#endif
